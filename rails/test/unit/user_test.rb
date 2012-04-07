c2bea6adc231aff1b7aaef9a91f82b6e0e3c15e1
charliesome <charlie@charliesomerville.com>
2012-04-07 14:35:55 +1000

    gravatar test

--- rails/test/unit/user_test.rb
+++ rails/test/unit/user_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "gravatar of a user is correct" do
+    assert_equal "http://www.gravatar.com/avatar/bc6a715808d9aae0ddeefb1e47e482a6", User.make(email: "john@doe.org").gravatar_url
+  end
 end
