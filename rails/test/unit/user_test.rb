cd3e8dac11f5de9045c8e47c1f02a278d4e98856
charliesome <charlie@charliesomerville.com>
2012-04-07 16:51:49 +1000

    test that old invites cannot be reused

--- rails/test/unit/user_test.rb
+++ rails/test/unit/user_test.rb
@@ -4,4 +4,12 @@ class UserTest < ActiveSupport::TestCase
   test "gravatar of a user is correct" do
     assert_equal "http://www.gravatar.com/avatar/bc6a715808d9aae0ddeefb1e47e482a6", User.make(email: "john@doe.org").gravatar_url
   end
+  
+  test "old invite cannot be reused" do
+    old_user = User.make!
+    new_user = User.make
+    new_user.invite_code = old_user.invite_code
+    refute new_user.save
+    assert new_user.errors[:invite_code]
+  end
 end
