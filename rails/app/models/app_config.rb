f0e5f9f56eb8284d43162acdeb978a446e6ffd8b
charliesome <charlie@charliesomerville.com>
2012-04-07 15:47:55 +1000

    change app_config to use method_missing magic becuase it looks nicer

--- rails/app/models/app_config.rb
+++ rails/app/models/app_config.rb
@@ -1,1 +1,24 @@
-AppConfig = YAML.load_file(File.expand_path("config/app_config.yml", Rails.root))[Rails.env] or raise "Couldn't load application configuration from config/app_config.yml"
+class AppConfigAccessor
+  def initialize(opts = {})
+    @opts = opts
+  end
+  
+  def [](key)
+    @opts[key.to_s]
+  end
+  
+  def method_missing(sym)
+    if self[sym].is_a? Hash
+      AppConfigAccessor.new self[sym]
+    else
+      self[sym]
+    end
+  end
+end
+
+filename = File.expand_path("config/app_config.yml", Rails.root)
+if File.exist? filename
+  AppConfig = AppConfigAccessor.new YAML.load(File.read filename)[Rails.env]
+else
+  raise "Couldn't load application configuration from config/app_config.yml"
+end
