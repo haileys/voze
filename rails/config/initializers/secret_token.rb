cf8b47af91be8ec5dd9b9edea0b46c9527a4add3
charliesome <charlie@charliesomerville.com>
2012-04-07 15:48:29 +1000

    change secret_token initializer to use method syntax for AppConfig

--- rails/config/initializers/secret_token.rb
+++ rails/config/initializers/secret_token.rb
@@ -2,4 +2,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Voze::Application.config.secret_token = AppConfig["secret_token"]
+Voze::Application.config.secret_token = AppConfig.secret_token
