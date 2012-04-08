6a8d1c339f3aa5b00b96d6cbe00724b0aa23204f
charliesome <charlie@charliesomerville.com>
2012-04-08 18:25:19 +1000

    allow smtp in configuration

--- rails/config/initializers/action_mailer.rb
+++ rails/config/initializers/action_mailer.rb
@@ -1,1 +1,8 @@
-Voze::Application.config.action_mailer.default_url_options = Hash[AppConfig[:default_url_options].map { |k,v| [k.intern, v] }]
+class Voze::Application
+  config.action_mailer.default_url_options = Hash[AppConfig[:default_url_options].map { |k,v| [k.intern, v] }]
+
+  if AppConfig[:smtp]
+    config.action_mailer.delivery_method :smtp
+    config.action_mailer.smtp_settings = Hash[AppConfig[:smtp].map { |k,v| [k.intern, v] }]
+  end
+end
