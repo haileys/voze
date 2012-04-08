78ad3ea35e5f5991e4da8f193bad3a35817610fc
charliesome <charlie@charliesomerville.com>
2012-04-08 14:21:51 +1000

    actually perform mail delivery

--- rails/config/environments/production.rb
+++ rails/config/environments/production.rb
@@ -48,8 +48,10 @@ Voze::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
-  # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  # Send email:
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable threaded mode
   config.threadsafe!
