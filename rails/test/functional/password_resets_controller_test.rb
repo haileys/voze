291a215ca5034506ddbf8b1b07ad86403c324d1c
charliesome <charlie@charliesomerville.com>
2012-04-09 13:56:40 +1000

    password resets controller test

--- rails/test/functional/password_resets_controller_test.rb
+++ rails/test/functional/password_resets_controller_test.rb
@@ -1,7 +1,44 @@
 require 'test_helper'
 
 class PasswordResetsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "can reset password if logged out" do
+    get :new
+    assert_response :success
+  end
+  
+  test "password reset acts successful even if an invalid username is used" do
+    post :create, password_reset: { username: "nope" }
+    assert_response :success
+    assert response.body.include? "has been emailed to you"
+  end
+  
+  test "password reset works with a username" do
+    user = User.make!
+    refute user.password_reset_token
+    post :create, password_reset: { username: user.username }
+    assert user.reload.password_reset_token
+  end
+  
+  test "password reset works with an email" do
+    user = User.make!
+    refute user.password_reset_token
+    post :create, password_reset: { username: user.email }
+    assert user.reload.password_reset_token
+  end
+  
+  test "password reset emails the user a reset token" do
+    user = User.make!
+    assert_difference "ActionMailer::Base.deliveries.count" do
+      post :create, password_reset: { username: user.username }
+    end
+    assert ActionMailer::Base.deliveries.last.encoded.include? user.reload.password_reset_token
+  end
+  
+  test "a successful password reset logs the user in and redirects" do
+    user = User.make!
+    user.generate_password_reset_token!
+    get :show, id: user.password_reset_token
+    assert_response :redirect
+    assert_equal user, @controller.current_user
+  end
 end
