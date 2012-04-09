4a9170182c549ea281b14df078f4c028741da0bc
charliesome <charlie@charliesomerville.com>
2012-04-09 18:21:57 +1000

    update user's ratio

--- rails/app/models/peer.rb
+++ rails/app/models/peer.rb
@@ -4,6 +4,14 @@ class Peer < ActiveRecord::Base
   belongs_to :user
   belongs_to :torrent
   
+  after_update :update_users_uploaded_downloaded
+  
+  def update_users_uploaded_downloaded
+    user.upload += [uploaded - uploaded_was, 0].max if uploaded_changed?
+    user.download += [downloaded - downloaded_was, 0].max if downloaded_changed?
+    user.save
+  end
+  
   def as_tracker_response
     { ip: ip, port: port, peer_id: peer_id.chars.each_slice(2).map { |x| x.join.to_i(16).chr }.join }
   end
