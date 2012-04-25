bab2f8b7cd88c79532582ff36ffeda08f78c7512
charliesome <charlie@charliesomerville.com>
2012-04-25 19:44:04 +1000

    drop downs

--- rails/app/assets/javascripts/application.js
+++ rails/app/assets/javascripts/application.js
@@ -13,3 +13,42 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(function() {
+
+    // placeholder polyfill
+    $('[placeholder]').focus(function() {
+    	var input = $(this);
+    	if (input.val() == input.attr('placeholder')) {
+    		input.val('');
+    		input.removeClass('placeholder');
+    	}
+    }).blur(function() {
+    	var input = $(this);
+    	if (input.val() == '' || input.val() == input.attr('placeholder')) {
+    		input.addClass('placeholder');
+    		input.val(input.attr('placeholder'));
+    	}
+    }).blur();
+
+    // dropdown
+    $('body > nav .icon').click(function(ev) {
+        ev.preventDefault();
+    	$('body > nav ul li.active').removeClass('active');
+    	$('body > nav ul li .container').hide();
+    	$(this).parent().addClass('active');
+    	$(this).parent().children('.container').show();
+    	//$('#overlay').show();
+    	return false;
+    });
+
+    $('body').click(function() {
+    	$('body > nav ul li.active').removeClass('active');
+    	$('body > nav ul li .container').hide();
+    	//$(this).hide();
+    });
+
+    // dud links
+    $('a[href=#]').click(function(){ return false; });
+
+});
