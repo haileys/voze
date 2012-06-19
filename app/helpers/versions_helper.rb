2bfbb78892f347792525275a0f7611b58d9ace90
Charlie Somerville <charlie@charliesomerville.com>
2012-06-19 19:04:36 +1000

    move personal_announce_url into application_helper

--- app/helpers/versions_helper.rb
+++ app/helpers/versions_helper.rb
@@ -1,8 +1,4 @@
 module VersionsHelper
-  def personal_announce_url
-    announce_url auth_token: current_user.auth_token
-  end
-
   def render_metadata(section)
     partial_name = "#{@torrent.metadata.class.name.underscore}/#{section}"
     render partial: partial_name, locals: { metadata: @torrent.metadata }
