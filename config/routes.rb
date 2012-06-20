6b4b37ce5096bae78870ce40ff07b21c0f93ffe6
Charlie Somerville <charlie@charliesomerville.com>
2012-06-20 18:49:42 +1000

    map /login to sessions#new

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Voze::Application.routes.draw do
   root to: "home#index"
   
   resource :sessions, only: [:new, :create, :destroy]
+  get "/login" => "sessions#new", as: :new_sessions
+  
   resources :users, only: [:show, :new, :create]
   resources :password_resets, only: [:show, :new, :create]
   resources :torrents, only: [:index, :show, :new, :create] do
