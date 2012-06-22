1b81d29d30a2bdd80ec9c84142c495abccb6a750
Charlie Somerville <charlie@charliesomerville.com>
2012-06-22 22:07:04 +1000

    torrent accepts one nested version

--- app/models/torrent.rb
+++ app/models/torrent.rb
@@ -1,10 +1,12 @@
 class Torrent < ActiveRecord::Base
-  attr_accessible :name, :about, :category_id
+  attr_accessible :name, :description, :category_id
   
   belongs_to :user
   belongs_to :category
   has_many :versions
   has_one :metadata
+  
+  accepts_nested_attributes_for :versions, limit: 1
   
   def to_param
     "#{id}-#{name.parameterize}"
